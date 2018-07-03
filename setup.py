d88e281d7fba9fd8604f3e782ddf0d69f4251b2a
Dave Gaeddert <dave.gaeddert@gmail.com>
2018-07-03 10:54:02 -0500

    Preparing release 0.0.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = (
 
 setup(
     name='combine',
-    version='0.0.4.dev0',
+    version='0.0.4',
     description='A helpful, simple static site generator.',
     long_description=long_description,
     long_description_content_type='text/markdown',
