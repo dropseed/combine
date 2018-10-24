0dfd38114c6315947c0bba3118f788e41c05383e
Dave Gaeddert <dave.gaeddert@gmail.com>
2018-10-24 15:37:19 -0500

    Back to development: 0.0.13

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = (
 
 setup(
     name='combine',
-    version='0.0.12',
+    version='0.0.13.dev0',
     description='A helpful, simple static site generator.',
     long_description=long_description,
     long_description_content_type='text/markdown',
