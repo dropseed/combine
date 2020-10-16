37179c5d58e95b4c107dbb27a55f38a8c8be5756
Dave Gaeddert <dave.gaeddert@gmail.com>
2020-10-16 10:43:26 -0500

    Add include_raw tag

--- combine/jinja/__init__.py
+++ combine/jinja/__init__.py
@@ -1,9 +1,10 @@
 from .code import CodeHighlightExtension
 from .markdown import MarkdownExtension
+from .include_raw import IncludeRawExtension
 from .urls import absolute_url
 
 
-default_extensions = [CodeHighlightExtension, MarkdownExtension]
+default_extensions = [CodeHighlightExtension, MarkdownExtension, IncludeRawExtension]
 default_filters = {
     "absolute_url": absolute_url,
 }
