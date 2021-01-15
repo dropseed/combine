57854d24764ea9028078a5dccb9ecc69225cb7c1
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-01-15 15:18:04 -0600

    Add img alt missing check

---
+++ tests/checks/snapshots/snap_test_img_alt.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_img_alt_check 1'] = [
+    {
+        'context': {
+            'element': '<img/>'
+        },
+        'type': 'img-alt-missing'
+    }
+]
