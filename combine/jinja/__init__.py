90d9cc1b7a02e00565eeb9d3e1b6369c7f24bf77
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-12-27 15:04:46 -0600

    Add |markdown filter

--- combine/jinja/__init__.py
+++ combine/jinja/__init__.py
@@ -1,5 +1,5 @@
 from .code import CodeHighlightExtension
-from .markdown import MarkdownExtension
+from .markdown import MarkdownExtension, markdown_filter
 from .include_raw import IncludeRawExtension
 from .urls import absolute_url
 
@@ -7,4 +7,5 @@ from .urls import absolute_url
 default_extensions = [CodeHighlightExtension, MarkdownExtension, IncludeRawExtension]
 default_filters = {
     "absolute_url": absolute_url,
+    "markdown": markdown_filter,
 }
