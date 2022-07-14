f53435ae6336eb7f2d273a5101c84f71e031c91c
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-07-14 12:36:33 -0500

    Load file references on init

--- combine/files/template.py
+++ combine/files/template.py
@@ -3,5 +3,8 @@ from .ignored import IgnoredFile
 
 
 class TemplateFile(IgnoredFile):
-    def _render_to_output(self, output_path, jinja_environment):
+    def load(self, jinja_environment):
         self.references = get_references_in_path(self.path, jinja_environment)
+
+    def _render_to_output(self, output_path, jinja_environment):
+        self.load(jinja_environment)
